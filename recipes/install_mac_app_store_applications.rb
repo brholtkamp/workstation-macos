908a6f5749688d3da322e325d1a173157b765fd4
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-12 03:14:35 -0500

    Working on MAS recipe

--- recipes/install_mac_app_store_applications.rb
+++ recipes/install_mac_app_store_applications.rb
@@ -24,11 +24,16 @@ node['workstation']['mas']['applications'].each do |app, fileName|
   end
 end
 
-mac_app_store_mas 'mas signout' do
-  source :homebrew
-  username node['workstation']['mas']['email']
-  password node['workstation']['mas']['password']
-  system_user node['workstation']['user']
-  use_rtun true
-  action :sign_out
+execute 'reattach-to-user-namespace mas signout' do
+  user node['workstation']['user']
 end
+
+# TODO: https://github.com/RoboticCheese/mac-app-store-chef/issues/41
+#mac_app_store_mas 'mas signout' do
+#  source :homebrew
+#  username node['workstation']['mas']['email']
+#  password node['workstation']['mas']['password']
+#  system_user node['workstation']['user']
+#  use_rtun true
+#  action :sign_out
+#end
