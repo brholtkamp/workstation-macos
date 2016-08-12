5593a861882942702571ae6c2f648a816f0a2d76
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-12 01:23:06 -0500

    Working on MAS recipe

--- recipes/install_mac_app_store_applications.rb
+++ recipes/install_mac_app_store_applications.rb
@@ -7,19 +7,21 @@
 include_recipe 'homebrew::default'
 
 homebrew_package 'mas'
+homebrew_package 'reattach-to-user-namespace'
 
 execute 'mas signin' do
-  command "mas signin #{node['workstation']['mas']['email']} '#{node['workstation']['mas']['password']}'"
+  command "reattach-to-user-namespace mas signin #{node['workstation']['mas']['email']} '#{node['workstation']['mas']['password']}'"
   user node['workstation']['user']
 end
 
 node['workstation']['mas']['applications'].each do |app|
-  execute "mas install #{app}" do
+  execute "reattach-to-user-namespace mas install #{app}" do
     user node['workstation']['user']
   end
 end
 
 execute 'mas signout' do
+  command 'reattach-to-user-namespace mas signout'
   sensitive true
   user node['workstation']['user']
 end
