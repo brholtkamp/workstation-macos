10c3d557f349ac20ca6842f3702bf1ef210f9a88
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-13 17:07:58 -0500

    Fixed attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,8 @@ default['workstation']['plists'] = []
 # Mac App Store
 default['workstation']['mas']['email'] = nil
 default['workstation']['mas']['password'] = nil
-default['workstation']['mas']['applications'] = [] # Use the number IDs of the applications
+default['workstation']['mas']['applications'] = [[]] # List of lists of ['Application name', 'Application.app filename']
 
-# Mac_os_x Settings
-default['mac_os_x']['settings_user'] = nil 
+# mac_os_x Settings
+default['mac_os_x']['settings_user'] = nil
 default['mac_os_x']['settings'] = {}
