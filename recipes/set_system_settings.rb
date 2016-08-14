c6352c5c3d8e4704a60ae3084552016fc1bd76bd
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-14 01:43:15 -0500

    Removed commad running

--- recipes/set_system_settings.rb
+++ recipes/set_system_settings.rb
@@ -4,10 +4,3 @@
 #
 
 include_recipe 'mac_os_x::settings'
-
-node['workstation']['commands'].each do |command|
-  execute "executing #{command}" do
-    command command
-    user node['workstation']['user']
-  end
-end
