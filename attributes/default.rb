d03ab28fd184fe858cf754f4203b8d21997046d9
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-14 00:38:57 -0500

    Added in one-off commands that don't fit the typical defaults situation

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,13 @@
 default['workstation']['applications'] = []
+default['workstation']['commands'] = []
 default['workstation']['games'] = []
 default['workstation']['plists'] = []
 
 # Mac App Store
+default['workstation']['mas']['applications'] = [[]] # List of lists of ['Application name', 'Application.app filename']
 default['workstation']['mas']['email'] = nil
 default['workstation']['mas']['password'] = nil
-default['workstation']['mas']['applications'] = [[]] # List of lists of ['Application name', 'Application.app filename']
 
 # mac_os_x Settings
-default['mac_os_x']['settings_user'] = nil
 default['mac_os_x']['settings'] = {}
+default['mac_os_x']['settings_user'] = nil
--- recipes/set_system_settings.rb
+++ recipes/set_system_settings.rb
@@ -4,3 +4,10 @@
 #
 
 include_recipe 'mac_os_x::settings'
+
+node['workstation']['commands'].each do |command|
+  execute "executing #{command}" do
+    command command
+    user node['workstation']['user']
+  end
+end
