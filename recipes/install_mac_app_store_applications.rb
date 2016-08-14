954e0c53a19161ef1940ce0138d1a70c7c2acc9a
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-14 01:30:59 -0500

    Fixed rubocop issues

--- recipes/install_mac_app_store_applications.rb
+++ recipes/install_mac_app_store_applications.rb
@@ -13,7 +13,7 @@ execute 'mas signin' do
   command "reattach-to-user-namespace mas signin #{node['workstation']['mas']['email']} '#{node['workstation']['mas']['password']}'"
   user node['workstation']['user']
   sensitive true
-  returns [0,7]
+  returns [0, 7]
 end
 
 execute 'mas upgrade' do
@@ -21,40 +21,39 @@ execute 'mas upgrade' do
   user node['workstation']['user']
 end
 
-node['workstation']['mas']['applications'].each do |number, fileName|
-  execute "mas install #{fileName}" do
+node['workstation']['mas']['applications'].each do |number, file_name|
+  execute "mas install #{file_name}" do
     command "reattach-to-user-namespace mas install #{number}"
     user node['workstation']['user']
-    not_if { ::Dir.exist?("/Applications/#{fileName}.app/") }
+    not_if { ::Dir.exist?("/Applications/#{file_name}.app/") }
   end
 end
 
-#mac_app_store_mas 'mas signin' do
-#  source :homebrew
-#  username node['workstation']['mas']['email']
-#  password node['workstation']['mas']['password']
-#  system_user node['workstation']['user']
-#  use_rtun true
-#  action %i(install sign_in upgrade upgrade_apps)
-#end
+#  TODO: https://github.com/RoboticCheese/mac-app-store-chef/issues/41
+# mac_app_store_mas 'mas signin' do
+#   source :homebrew
+#   username node['workstation']['mas']['email']
+#   password node['workstation']['mas']['password']
+#   system_user node['workstation']['user']
+#   use_rtun true
+#   action %i(install sign_in upgrade upgrade_apps)
+# end
 #
-#node['workstation']['mas']['applications'].each do |app, fileName|
-#  mac_app_store_app "install #{app}" do
-#    app_name app
-#    system_user node['workstation']['user']
-#    use_rtun true
-#    action :install
-#    not_if { ::Dir.exist?("/Applications/#{fileName}.app/") }
-#  end
-#end
+# node['workstation']['mas']['applications'].each do |app, fileName|
+#   mac_app_store_app "install #{app}" do
+#     app_name app
+#     system_user node['workstation']['user']
+#     use_rtun true
+#     action :install
+#     not_if { ::Dir.exist?("/Applications/#{fileName}.app/") }
+#   end
+# end
 #
-#
-# TODO: https://github.com/RoboticCheese/mac-app-store-chef/issues/41
-#mac_app_store_mas 'mas signout' do
-#  source :homebrew
-#  username node['workstation']['mas']['email']
-#  password node['workstation']['mas']['password']
-#  system_user node['workstation']['user']
-#  use_rtun true
-#  action :sign_out
-#end
+# mac_app_store_mas 'mas signout' do
+#   source :homebrew
+#   username node['workstation']['mas']['email']
+#   password node['workstation']['mas']['password']
+#   system_user node['workstation']['user']
+#   use_rtun true
+#   action :sign_out
+# end
