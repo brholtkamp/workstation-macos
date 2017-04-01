ae0db3b51598e9f8105f977bc75f5b4c977607db
Brian Holtkamp <brholtkamp@gmail.com>
2017-03-31 20:45:42 -0500

    Removed system_user argument

--- recipes/install_mac_app_store_applications.rb
+++ recipes/install_mac_app_store_applications.rb
@@ -13,7 +13,6 @@ mac_app_store_mas 'mas signin' do
   source :homebrew
   username node['workstation']['mas']['email']
   password node['workstation']['mas']['password']
-  system_user node['workstation']['user']
   use_rtun true
   action %i(install sign_in upgrade upgrade_apps)
 end
@@ -21,9 +20,8 @@ end
 node['workstation']['mas']['applications'].each do |app, fileName|
   mac_app_store_app "install #{app}" do
     app_name app
-    system_user node['workstation']['user']
-    use_rtun true
     action :install
+    use_rtun true
     not_if { ::Dir.exist?("/Applications/#{fileName}.app/") }
   end
 end
